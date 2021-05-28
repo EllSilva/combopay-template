062b325676c9f9df8e11320a1a7aa219d92591ef
brunocriacoes <br.rafael@outlook.com>
2021-05-28 17:10:17 -0300

    fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,14 +20,8 @@ globalThis.app = new Vue({
             backgoundImage: './sass/doacoesbethania.com.br/galery/1.jpg',
         },
         urlVideo: 'https://www.youtube.com/watch?v=H-GrnWRc2i4',
-        videos: [
-            'https://www.youtube.com/watch?v=NatrfQRSJMU',
-            'https://www.youtube.com/watch?v=wacNNeEpUfo',
-        ],
-        depoimento_video: [
-            'https://www.youtube.com/watch?v=FRSMONLcaqE',
-            'https://www.youtube.com/watch?v=1EKUno_uRfM',
-        ],
+        videos: [],
+        depoimento_video: [],
         galerys: [
             { src: './sass/doacoesbethania.com.br/galery/1.jpg', title: 'Dinâmicas de Grupo' },
             { src: './sass/doacoesbethania.com.br/galery/2.jpg', title: 'Momentos de Partilhas' },
@@ -38,6 +32,15 @@ globalThis.app = new Vue({
             { src: './sass/doacoesbethania.com.br/galery/7.jpg', title: 'Virtude da Disciplina' },
             { src: './sass/doacoesbethania.com.br/galery/8.jpg', title: 'Adoração ao Santíssimo' },
         ],
+        layout: {
+            title_depoimento: "?",
+            title_video: "?",
+            title_galeria: "?",
+            decription_galeria: "?",
+            sitacao: "?",
+            bio: "?",
+            copy: "?"
+        },
         galery_title: null,
         pop_id: 'QpxA_ZxGX_M',
         pop_status: false,
@@ -74,6 +77,10 @@ globalThis.app = new Vue({
             this.videos.push(this.urlVideo)
             this.put_videos()
         },
+        addDepoimentos() {
+            this.depoimento_video.push(this.urlVideo)
+            this.put_depoimento_videos()
+        },
         getDomain() {
             return window.location.hostname
         },
@@ -101,6 +108,23 @@ globalThis.app = new Vue({
                 instituicao_id: this.institution_id
             })
         },
+        async put_depoimento_videos() {
+            let res = await this.Super.flag_put(this.flagsIds.DEPOIMENTOS, {
+                base64: btoa(JSON.stringify(this.depoimento_video)),
+                flag: 'DEPOIMENTOS',
+                instituicao_id: this.institution_id
+            })
+        },
+        async put_layout(e) {
+            let name = e.target.getAttribute('name')
+            this.layout[name] = e.target.innerText
+            let res = await this.Super.flag_put(this.flagsIds.LAYOUT, {
+                base64: btoa(JSON.stringify(this.layout)),
+                flag: 'LAYOUT',
+                instituicao_id: this.institution_id
+            })
+        },
+        
 
     },
     filters: {
@@ -119,11 +143,12 @@ globalThis.app = new Vue({
         this.flagsIds = this.Domain.flags_ids(all_flags)
         this.flags = this.Domain.render_flag(all_flags)
 
-        console.log( this.flagsIds )
 
         this.exist_flags()
         
         this.videos = this.flags.VIDEOS
+        this.depoimento_video = this.flags.DEPOIMENTOS
+        this.layout = { ...this.layout, ...this.flags.LAYOUT}
 
 
     }
