eed225f4fdbd320eb517c91b24cf7505be15f833
brunocriacoes <br.rafael@outlook.com>
2021-07-22 16:03:35 -0300

    fix

---
+++ painel/assets/js/mask/printPdf.js
@@ -0,0 +1,16 @@
+export default function (url) {
+    var iframe = document.createElement('iframe');
+    iframe.className = 'pdfIframe'
+    document.body.appendChild(iframe);
+    iframe.style.display = 'none';
+    iframe.onload = function () {
+        setTimeout(function () {
+            iframe.focus();
+            iframe.contentWindow.print();
+            URL.revokeObjectURL(url)
+            // document.body.removeChild(iframe)
+        }, 1);
+    };
+    iframe.src = url;
+    // URL.revokeObjectURL(url)
+}
